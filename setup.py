ea590ec7f7ebbb2a4162657289cede92f380a770
Kiran Kumar Kotari <kirankotari@live.com>
2020-03-05 11:56:54 +0530

    requirements.txt path correction

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError: # for pip <= 9.0.3
     from pip.req import parse_requirements
 
 here = path.abspath(path.dirname(__file__))
-install_reqs = parse_requirements('requirements.txt', session='hack')
+install_reqs = parse_requirements(path.join(here, 'requirements.txt'), session='hack')
 reqs = [str(ir.req) for ir in install_reqs]
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
 	name='pingping',
-	version='1.2.6',
+	version='1.2.7',
 	description="pingping is a special library which understands multi linguistic of ping output and translated the result to machine understandable format.",
 	long_description = long_description,
 	long_description_content_type = 'text/markdown',
